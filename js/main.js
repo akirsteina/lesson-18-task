ad91aafb318c8e16b01a72b9b463593990bee99d
akirsteina <akirsteina@users.noreply.github.com>
2021-03-15 21:31:38 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // task 1 - fizzbazz
-console.log('Task 3: Fizz Bazz for numbers from 0 to 100')
+console.log('Task 1: Fizz Bazz for numbers from 0 to 100');
 for (let i = 0; i <= 100; i++) {
     if (i % 2 === 0 && i % 3 === 0) {
         console.log('Fizz Bazz');
@@ -13,6 +13,7 @@ for (let i = 0; i <= 100; i++) {
 }
 
 // task 2 - every 7th number from 1000 to 50 - odd/even
+console.log('Task 2: count backwards from 100 to 50 by 7');
 for (let i = 1000; i >= 50; i -= 7) {
     if (i % 2) {
         console.log(i, ' is an odd number');
